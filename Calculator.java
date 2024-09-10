6fca3cbc3a820d57fa875662cab08fb7c88549f4
JohnPaul <devega.johnpaul12@gmail.com>
2024-09-10 10:54:10 +0800

    Add subtraction method

--- Calculator.java
+++ Calculator.java
@@ -3,8 +3,10 @@ public class calculator {
         int a = 2;
         int b = 4;
         int sum = a + b;
+        int sub = a - b;
 
         System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + sub);
 
         }
     }
